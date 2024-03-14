506872572a4b9bfe72ff4b966851ad7b2a5d9d50
Flavia <flavinharamendes@gmail.com>
2024-03-14 17:10:52 -0300

    primeiro metodo

---
+++ ativ1array.js
@@ -0,0 +1,9 @@
+//Inicializando a atividade 
+
+//Metodo 1: push 
+//push: adiciona na última posição do vetor 
+let acessorios = ['pulseira, colar, brinco, anel'];
+acessorios.push('tornozeleira');
+acessorios.push('tiara');
+acessorios.push('luva');
+console.log(acessorios);
